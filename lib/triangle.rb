b9b3c9dfcb3c9b78af9aa3622134d6711527a565
Raul Sposito <raulsposito@gmail.com>
2019-10-26 18:37:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
     validate_triangle
     if a == b && b == c 
       :equilateral 
-      elsif a == b || b == c || 
+      elsif a == b || b == c || a == c 
   
   
   def validate_triangle 
