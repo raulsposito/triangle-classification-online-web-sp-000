67d7b2a276514c8879c2064d5dd9c953de95d5dc
Raul Sposito <raulsposito@gmail.com>
2019-10-26 18:35:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,6 +14,6 @@ class Triangle
   def validate_triangle 
     real_triangle = [(a + b > c), (a + c > b), (b + c > a)] 
     [a, b, c].each { |s| real_triangle << false if s <= 0 }
-    
+    raise TriangleError 
   
 end
