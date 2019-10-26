1d1d3c8e926106622ece26658e208176bb51efa9
Raul Sposito <raulsposito@gmail.com>
2019-10-26 18:04:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,8 @@ class Triangle
   class TriangleError < StandardError
   end
   
+  attr_accessor 
+  
   def initialize(attributes)
     attributes.each {|lenght, value| self.send(("#{lenght}="), value)
   end
