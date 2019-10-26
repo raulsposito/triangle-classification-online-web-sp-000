4b314124b2a703874b12673b947961a26dca288d
Raul Sposito <raulsposito@gmail.com>
2019-10-26 18:04:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,7 @@ class Triangle
   end
   
   def initialize(attributes)
-    attributes.each {|lenght, value| self.send(("#{lenght}"))
+    attributes.each {|lenght, value| self.send(("#{lenght}="))
     
   end
 
