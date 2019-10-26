40a82a7b88facdcac18c32abbd7d7fd862d02bff
Raul Sposito <raulsposito@gmail.com>
2019-10-26 18:36:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,6 @@ class Triangle
     real_triangle = [(a + b > c), (a + c > b), (b + c > a)] 
     [a, b, c].each { |s| real_triangle << false if s <= 0 }
     raise TriangleError if real_triangle.include?(false)
-    
+  end
   
 end
